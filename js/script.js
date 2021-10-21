5cb37325a6ba002b796303b2e2baded3fe78325c
Rc-Rebello <raphaelcrebello@gmail.com>
2021-10-20 23:43:47 -0300

    Cria comida da cobra em posições aleatórias.

--- js/script.js
+++ js/script.js
@@ -8,6 +8,10 @@ snake[0] = {
 }
 
 let direction = "right"
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBg() {
     context.fillStyle = "lightgreen"
@@ -21,6 +25,11 @@ function criarCobra() {
     }
 }
 
+function drawFood() {
+    context.fillStyle = "orange";
+    context.fillRect(food.x, food.y, box, box);
+
+}
 document.addEventListener('keydown', update);
 
 function update(event) {
@@ -31,8 +40,7 @@ function update(event) {
 
 }
 
-function
-iniciarJogo() {
+function iniciarJogo() {
     if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if (snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
     if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
@@ -40,6 +48,7 @@ iniciarJogo() {
 
     criarBg();
     criarCobra();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
